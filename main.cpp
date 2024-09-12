e7153fc5206ac196c7345047ad0e4b370764fe04
s1qw1ck <tarasgizimchuk@gmail.com>
2024-09-12 09:56:12 +0200

    ini

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
- int main() {
-   std::cout << "Hello world";
-   
+#include <cstring>
+using namespace std;
+int main(){
+    setlocale(LC_ALL,"RU");
+
+    float num1,num2,res;
+    cout << "Enter number1: " << endl;
+    cin >> num1;
+
+    cout << "Enter number2: ";
+    cin >> num2;
+
+    string math;
+    cout << "Enter math symbol: ";
+    cin >> math;
+
+    if(math == "+")
+    {res = num1 + num2;}
+
+    else if(math == "-")
+    {res = num1 - num2;}
+
+    else if(math == "*")
+    {res = num1 * num2;}
+
+    else if(math == "/")
+    {res = num1 / num2;}
+    cout << "Result: " << res << endl;
 
 
- }
+    return 0;
+}
+
